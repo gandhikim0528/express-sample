b4a4e44109080b015494a6f4bd3e460f2ffae0c4
blockodyssey-bckim <gandhikim0528@gmail.com>
2020-04-10 09:55:54 +0900

    debug sample

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+//var debug = require('debug')('myapp:index');
+var debug = require('debug');
+var info = debug('myapp:info');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  debug('debug log test')
+  info('info log test')
   res.render('index', { title: 'Express' });
 });
 
